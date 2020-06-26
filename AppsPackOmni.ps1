f2b0d696d76f0cd5c22bbc2e8d1bdf28d9c4ba69
Donovan <46878790+pctopgs@users.noreply.github.com>
2020-06-26 06:19:10 -0500

    Testing ode
    Code didn't work like I thought, so commenting out most things and testing

--- AppsPackOmni.ps1
+++ AppsPackOmni.ps1
@@ -1,35 +1,75 @@
 Write-Host "Installing Chocolatey..."
 
+
+<#
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install seatools windirstat 7zip putty wireshark vscode visualstudio2015community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider nmap -y
+#>
 
 
 Write-Host "Is this a:"
+
 Write-Host "1. Workstation"
+
 Write-Host "2. Laptop"
+
 Write-Host "3. Tablet"
+
 Write-Host "4. Small Tablet"
+
 Write-Host "5. Server"
+
+write-host "6. Exit"
+
 
 $choice = Read-Host -Prompt "Choice: "
 
+
+
 if ($choice -eq 1) # Workstation
+
 {
-    choco install seatools windirstat 7zip putty wireshark vscode visualstudio2019community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge shotcut github-desktop krita tor-browser choco install sumatrapdf -y
+    write-host "Workstation"
+    choco install seatools windirstat 7zip putty wireshark vscode <#visualstudio2019community#> libreoffice-fresh vlc gimp notepadplusplus microsoft-edge shotcut github-desktop krita tor-browser choco install sumatrapdf -y
+
 }
+
 elseif($choice -eq 2) # Laptop
+
 {
-    choco install seatools windirstat 7zip putty wireshark vscode visualstudio2019community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider shotcut github-desktop tor-browser choco install sumatrapdf -y
+    write-host "Laptop"
+    #choco install seatools windirstat 7zip putty wireshark vscode visualstudio2019community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider shotcut github-desktop tor-browser choco install sumatrapdf -y
+
 }
+
 elseif($choice -eq 3) # Tablet
+
 {
-    choco install seatools windirstat 7zip putty wireshark vscode visualstudio2019community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider-dev nmap choco install sumatrapdf -y
+    write-host "Tablet"
+    #choco install seatools windirstat 7zip putty wireshark vscode visualstudio2019community libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider-dev nmap choco install sumatrapdf -y
+
 }
+
 elseif($choice -eq 4) # Small Tablet
+
 {
-    choco install seatools windirstat 7zip putty wireshark vscode libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider-dev shotcut github-desktop -y
-}y
-elseif($choice -eq 5) # Media Server
-{
-    choco install seatools windirstat 7zip putty wireshark jellyfin sonarr radarr deluge -y
+    write-host "Small Tablet"
+    #choco install seatools windirstat 7zip putty wireshark vscode libreoffice-fresh vlc gimp notepadplusplus microsoft-edge-insider-dev shotcut github-desktop -y
+
 }
+
+elseif($choice -eq 5) # Media Server
+
+{
+    write-host "Media Server"
+    #choco install seatools windirstat 7zip putty wireshark jellyfin sonarr radarr deluge -y
+
+}
+
+elseif($choice -eq 0)
+{
+    write-host "\nGoodbye"
+}
+
+write-host "\nDone"
